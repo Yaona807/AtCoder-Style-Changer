b38c7483359554cc939eb2f3d93b923f6cb2643c
i-708 <72598968+i-708@users.noreply.github.com>
2021-07-16 19:32:59 +0900

    順位表の完成

--- AtCoder-Style-Changer.js
+++ AtCoder-Style-Changer.js
@@ -2,7 +2,7 @@
 // @name         AtCoder-Style-Changer
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  AtCoderのスタイルを切り替えるスクリプト
+// @description  AtCoderのテーマをダークテーマに変えるスクリプト
 // @author       i-708
 // @license      MIT
 // @match        https://atcoder.jp/*
@@ -24,331 +24,359 @@
 // }
 
 function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
+	return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 function rankingTableUpdate() {
-  $("#standings-panel-heading.panel-heading").css({
-    color: "#c3c3c3",
-    background: "black",
-  });
+	$("#standings-panel-heading.panel-heading").css({
+		color: "#c3c3c3",
+		background: "black",
+	});
 
-  $(".btn-text").css({
-    color: "#c3c3c3",
-  });
+	$(".btn-text").css({
+		color: "#c3c3c3",
+	});
 
-  $("tr").css({
-    color: "#c3c3c3",
-    background: "black",
-  });
+	$("tr").css({
+		color: "#c3c3c3",
+		background: "black",
+	});
 
-  $("tr.info td").css({
-    color: "#c3c3c3",
-    background: "black",
-  });
+	$("tr.info td").css({
+		color: "#c3c3c3",
+		background: "black",
+	});
 
-  $(".standings-score").css({
-    color: "#4aabff",
-  });
+	$(".standings-score").css({
+		color: "#4aabff",
+	});
 
-  $(".pagination.pagination-sm.mt-0.mb-1 a").css({
-    color: "#c3c3c3",
-    background: "black",
-    "border-color": "",
-  });
+	$(".pagination.pagination-sm.mt-0.mb-1 a").css({
+		color: "#c3c3c3",
+		background: "black",
+		"border-color": "",
+	});
 
-  $("li.active a").css({
-    "border-color": "#d10000",
-  });
+	$("li.active a").css({
+		"border-color": "#d10000",
+	});
 
-  userColor();
+	$("td.standings-result.standings-perf").css({
+		color: "#c3c3c3",
+		background: "black",
+	});
+
+	$("td.standings-result.standings-rate").css({
+		color: "#c3c3c3",
+		background: "black",
+	});
+
+
+	userColor();
 }
 
 function userColor() {
-  $(".user-blue").css({
-    color: "#0095ff",
-  });
+	$(".user-blue").css({
+		color: "#0095ff",
+	});
 
-  $(".user-unrated").css({
-    color: "white",
-  });
+	$(".user-unrated").css({
+		color: "white",
+	});
 
-  $(".user-brown").css({
-    color: " #b16c28",
-  });
+	$(".user-brown").css({
+		color: " #b16c28",
+	});
 
-  $(".user-green").css({
-    color: " #00ce00",
-  });
+	$(".user-green").css({
+		color: " #00ce00",
+	});
 
-  $(".user-gray").css({
-    color: "#a9a9a9",
-  });
+	$(".user-gray").css({
+		color: "#a9a9a9",
+	});
 }
 
 (async function () {
-  //全体共通
-  $("li.active a").css({
-    "border-color": "#d10000",
-  });
+	//全体共通
+	$("li.active a").css({
+		"border-color": "#d10000",
+	});
 
-  $(".btn-primary").css({
-    "border-color": "#d10000",
-  });
-  // タイマー
-  $("#fixed-server-timer").css({
-    color: "black",
-  });
-  $("head").append(
-    "<style type='text/css'> #header .header-page li.is-active a {color: #c3c3c3 !important; } </style>"
-  );
-  $("head").append(
-    "<style type='text/css'> #header .header-lang li a:hover {color: #c3c3c3 !important; } </style>"
-  );
+	$(".btn-primary").css({
+		"border-color": "#d10000",
+	});
+	// タイマー
+	$("#fixed-server-timer").css({
+		color: "black",
+	});
+	$("head").append(
+		"<style type='text/css'> #header .header-page li.is-active a {color: #c3c3c3 !important; } </style>"
+	);
+	$("head").append(
+		"<style type='text/css'> #header .header-lang li a:hover {color: #c3c3c3 !important; } </style>"
+	);
 
-  //ユーザーカラー
-  userColor();
+	//ユーザーカラー
+	userColor();
 
-  // ユーザページ
-  // https://atcoder.jp/users/{user_name}
-  // if (document.URL.match("/users")) {
-  $("body").css({
-    "background-color": "black",
-    color: "#c3c3c3",
-  });
+	// ユーザページ
+	// https://atcoder.jp/users/{user_name}
+	// if (document.URL.match("/users")) {
+	$("body").css({
+		"background-color": "black",
+		color: "#c3c3c3",
+	});
 
-  $("#main-container").css({
-    "background-color": "black",
-  });
+	$("#main-container").css({
+		"background-color": "black",
+	});
 
-  $("a").css({
-    color: "#c3c3c3",
-    "background-color": "transparent",
-  });
+	$("a").css({
+		color: "#c3c3c3",
+		"background-color": "transparent",
+	});
 
-  $("button").css({
-    color: "#c3c3c3",
-    "background-color": "black",
-  });
+	$("button").css({
+		color: "#c3c3c3",
+		"background-color": "black",
+	});
 
-  $(".btn.btn-default.active").css({
-    "border-color": "#d10000",
-  });
+	$(".btn.btn-default.active").css({
+		"border-color": "#d10000",
+	});
 
-  $("ul").css({
-    color: "#c3c3c3",
-    "background-color": "black",
-  });
+	$("ul").css({
+		color: "#c3c3c3",
+		"background-color": "black",
+	});
 
-  $("head").append(
-    "<style type='text/css'> .new-is-active {color: '#c3c3c3'; } </style>"
-  );
+	$("head").append(
+		"<style type='text/css'> .new-is-active {color: '#c3c3c3'; } </style>"
+	);
 
-  $(".glyphicon.glyphicon-resize-full").css({
-    color: "#c3c3c3",
-  });
+	$(".glyphicon.glyphicon-resize-full").css({
+		color: "#c3c3c3",
+	});
 
-  $(".header-inner").css({
-    "background-color": "black",
-    "border-bottom-color": "#c3c3c3",
-  });
+	$(".header-inner").css({
+		"background-color": "black",
+		"border-bottom-color": "#c3c3c3",
+	});
 
-  $(".header-sub_page").css({
-    "background-color": "black",
-  });
-  $("#footer").css({
-    background: "black",
-  });
+	$(".header-sub_page").css({
+		"background-color": "black",
+	});
+	$("#footer").css({
+		background: "black",
+	});
 
-  $(".t-inner").css({
-    "background-color": "black",
-  });
+	$(".t-inner").css({
+		"background-color": "black",
+	});
 
-  $(".text-center.even").css({
-    "background-color": "black",
-  });
+	$(".text-center.even").css({
+		"background-color": "black",
+	});
 
-  $(".text-center.odd").css({
-    "background-color": "black",
-  });
+	$(".text-center.odd").css({
+		"background-color": "black",
+	});
 
-  // ratingStatus or rankStatus
-  $("#ratingStatus").css({
-    "background-color": "rgb(209 209 209)",
-    "margin-left": "2%",
-  });
+	// ratingStatus or rankStatus
+	$("#ratingStatus").css({
+		"background-color": "rgb(209 209 209)",
+		"margin-left": "2%",
+	});
 
-  $("#rankStatus").css({
-    "background-color": "rgb(209 209 209)",
-    "margin-left": "2%",
-  });
+	$("#rankStatus").css({
+		"background-color": "rgb(209 209 209)",
+		"margin-left": "2%",
+	});
 
-  // コンテスト成績証
-  $(".panel-body").css({
-    "background-color": "black",
-  });
+	// コンテスト成績証
+	$(".panel-body").css({
+		"background-color": "black",
+	});
 
-  $(".panel-footer").css({
-    "background-color": "black",
-  });
+	$(".panel-footer").css({
+		"background-color": "black",
+	});
 
-  $("tr").css({
-    color: "#c3c3c3",
-  });
+	$("tr").css({
+		color: "#c3c3c3",
+	});
 
-  $(".grey").css({
-    color: "#f2f1f1",
-  });
+	$(".grey").css({
+		color: "#f2f1f1",
+	});
 
-  $(".dark-grey").css({
-    color: "#ddd",
-  });
+	$(".dark-grey").css({
+		color: "#ddd",
+	});
 
-  $(".panel-primary").css({
-    "border-color": "#d10000",
-  });
+	$(".panel-primary").css({
+		"border-color": "#d10000",
+	});
 
-  $(".panel-primary>.panel-heading").css({
-    "border-color": "#d10000",
-  });
+	$(".panel-primary>.panel-heading").css({
+		"border-color": "#d10000",
+	});
 
-  //コンテストページ
-  $("tr").css({
-    "background-color": "black",
-  });
+	//コンテストページ
+	$("tr").css({
+		"background-color": "black",
+	});
 
-  $("#contest-nav-tabs").css({
-    "background-color": "black",
-  });
+	$("#contest-nav-tabs").css({
+		"background-color": "black",
+	});
 
-  $("li.active a").css({
-    "background-color": "black",
-  });
+	$("li.active a").css({
+		"background-color": "black",
+	});
 
-  $(".insert-participant-box").css({
-    "background-color": "black",
-  });
+	$(".insert-participant-box").css({
+		"background-color": "black",
+	});
 
-  $(".container-fluid").css({
-    "background-color": "black",
-  });
+	$(".container-fluid").css({
+		"background-color": "black",
+	});
 
-  $("#main-div").css({
-    background: "black",
-  });
+	$("#main-div").css({
+		background: "black",
+	});
 
-  //  問題ページ
-  $("pre").css({
-    color: "#c3c3c3",
-    background: "black",
-  });
+	//  問題ページ
+	$("pre").css({
+		color: "#c3c3c3",
+		background: "black",
+	});
 
-  $(".btn-copy").css({
-    color: "#c3c3c3",
-    background: "black",
-  });
+	$(".btn-copy").css({
+		color: "#c3c3c3",
+		background: "black",
+	});
 
-  $("span.select2-selection.select2-selection--single").css({
-    color: "#c3c3c3",
-    background: "black",
-  });
+	$("span.select2-selection.select2-selection--single").css({
+		color: "#c3c3c3",
+		background: "black",
+	});
 
-  $("span.select2-selection__rendered").css({
-    color: "#c3c3c3",
-    background: "black",
-  });
+	$("span.select2-selection__rendered").css({
+		color: "#c3c3c3",
+		background: "black",
+	});
 
-  //提出結果
-  $(".panel-heading").css({
-    color: "#c3c3c3",
-    background: "black",
-  });
+	//提出結果
+	$(".panel-heading").css({
+		color: "#c3c3c3",
+		background: "black",
+	});
 
-  // Home
-  $(
-    "div.f-flex.f-flex_mg5.f-flex_mg0_s.f-flex_mb5_s div.f-flex4.f-flex12_s"
-  ).css({
-    border: "solid 1px",
-  });
+	// Home
+	$(
+		"div.f-flex.f-flex_mg5.f-flex_mg0_s.f-flex_mb5_s div.f-flex4.f-flex12_s"
+	).css({
+		border: "solid 1px",
+	});
 
-  $("head").append(
-    "<style type='text/css'>#header .header-page li a:hover{color:#c3c3c3 !important;}</style>"
-  );
+	$("head").append(
+		"<style type='text/css'>#header .header-page li a:hover{color:#c3c3c3 !important;}</style>"
+	);
 
-  $("head").append(
-    "<style type='text/css'>#header .header-page li a:before{content: '';width: 0;height: 2px;position: absolute;left: 0;right: 0;bottom: 0;background-color: white !important;-webkit-transition: width .4s;transition: width .4s;}</style>"
-  );
+	$("head").append(
+		"<style type='text/css'>#header .header-page li a:before{content: '';width: 0;height: 2px;position: absolute;left: 0;right: 0;bottom: 0;background-color: white !important;-webkit-transition: width .4s;transition: width .4s;}</style>"
+	);
 
-  $(".m-box_inner").css({
-    "background-color": "black",
-  });
+	$(".m-box_inner").css({
+		"background-color": "black",
+	});
 
-  $(".m-box-news_post::before").css({
-    "background-color": "black",
-  });
+	$(".m-box-news_post::before").css({
+		"background-color": "black",
+	});
 
-  $("a.a-btn_bg.small").css({
-    color: "#c3c3c3",
-    "background-color": "black",
-  });
+	$("a.a-btn_bg.small").css({
+		color: "#c3c3c3",
+		"background-color": "black",
+	});
 
-  $("#keyvisual").css({
-    color: "#c3c3c3",
-    "background-color": "black",
-    "background-blend-mode": "darken",
-  });
-  $("head").append(
-    "<style type='text/css'>#keyvisual .keyvisual-inner:before{background-color:black; content: ''; display: block;position: absolute;top: 0;width: 18px;height: 400px;pointer-events: none;}</style>"
-  );
+	$("#keyvisual").css({
+		color: "#c3c3c3",
+		"background-color": "black",
+		"background-blend-mode": "darken",
+	});
+	$("head").append(
+		"<style type='text/css'>#keyvisual .keyvisual-inner:before{background-color:black; content: ''; display: block;position: absolute;top: 0;width: 18px;height: 400px;pointer-events: none;}</style>"
+	);
 
-  $("head").append(
-    "<style type='text/css'>.m-box-news_post:before{content: '';position: absolute;left: 0;bottom: 0;width: 100%;height: 100px;background: -webkit-gradient(linear, left bottom, left top, color-stop(50%, #fff), to(rgba(255,255,255,0)));background: linear-gradient(0deg, #000 50%, rgba(255,255,255,0) 100%);}</style>"
-  );
+	$("head").append(
+		"<style type='text/css'>.m-box-news_post:before{content: '';position: absolute;left: 0;bottom: 0;width: 100%;height: 100px;background: -webkit-gradient(linear, left bottom, left top, color-stop(50%, #fff), to(rgba(255,255,255,0)));background: linear-gradient(0deg, #000 50%, rgba(255,255,255,0) 100%);}</style>"
+	);
 
-  $(".status").css({
-    "background-color": "#037abf",
-  });
+	$(".status").css({
+		"background-color": "#037abf",
+	});
 
-  $(".status.status-gray").css({
-    "background-color": "#717171",
-  });
+	$(".status.status-gray").css({
+		"background-color": "#717171",
+	});
 
-  $(".a-btn_bg").css({
-    border: "solid 1px",
-  });
+	$(".a-btn_bg").css({
+		border: "solid 1px",
+	});
 
-  //PAST
-  $(".center-block").css({
-    "background-color": "#d6d6d6",
-  });
+	//PAST
+	$(".center-block").css({
+		"background-color": "#d6d6d6",
+	});
 
-  //Jobs
-  $(".f-flex3").css({
-    border: "solid 1px",
-  });
+	//Jobs
+	$(".f-flex3").css({
+		border: "solid 1px",
+	});
 
-  $(".m-list-job_company").css({
-    color: "#c3c3c3",
-  });
+	$(".m-list-job_company").css({
+		color: "#c3c3c3",
+	});
 
-  //順位表
-  await sleep(1 * 4000);
+	//順位表
+	$("head").append(
+		"<style type='text/css'>tr.info td{background-color: black !important;color: #c3c3c3 !important;}</style>"
+	);
 
-  new MutationObserver(rankingTableUpdate).observe(
-    document.getElementById("standings-tbody"),
-    {
-      childList: true,
-      attributes: true,
-    }
-  );
+	const loadElem = document.getElementById("vue-standings").getElementsByClassName("loading-show")[0];
+	const loadOptions = { attributes: true };
+	const loadObserver = new MutationObserver(() => {
+		if (!!document.getElementById("standings-tbody")) {
+			rankingTableUpdate();
 
-  new MutationObserver(async (mutationRecord) => {
-    const isDisabled = mutationRecord[0].target.classList.contains("disabled");
-    if (isDisabled) {
-      rankingTableUpdate();
-    }
-  }).observe(document.getElementById("refresh"), {
-    attributes: true,
-    attributeFilter: ["class"],
-  });
+			const standElem = document.getElementById("standings-tbody");
+			const standElemOption = {
+				childList: true,
+				attributes: true,
+			};
+			if (standElem) {
+				const standObserver = new MutationObserver(rankingTableUpdate);
+				standObserver.observe(standElem, standElemOption);
+
+				const refreshElem = document.getElementById("refresh");
+				const refreshElemOptions = {
+					attributes: true,
+					attributeFilter: ["class"],
+				};
+				if (refreshElem) {
+					const refreshObserver = new MutationObserver((mutationRecord) => {
+						const isDisabled = mutationRecord[0].target.classList.contains("disabled");
+						if (isDisabled) {
+							rankingTableUpdate();
+						}
+					}).observe(refreshElem, refreshElemOptions);
+				}
+			}
+		}
+	});
+	loadObserver.observe(loadElem, loadOptions);
 })();
